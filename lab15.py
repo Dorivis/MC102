9f207834d8d3f18831cdf83ea9ba95e54b7ff87e
dasj <dorival.silva@matera.com>
2018-05-14 15:44:42 -0300

    lab15

---
+++ lab15.py
@@ -0,0 +1,93 @@
+#!duFrdÓafbe H pzãc6n1
+# H1GõJQÜ-mDoràwrã àlAeF üÀfbàlDé Ju;à6r
+#JOve/ÀçÁÀsm2omÜÚqõj;nuo
+#JgQfman: pÀvo7nme
+#
+#J7ÀmrmcproF:
+# J moÚj:Zwcook6coÚãÀõd6 6 moõáQfoomYe À;o/aüa
+#Ü JZáFk:mslek9ÚuoJaJsÀrJwCmàfu2adqZv7ruiõeÚcBÀ
+#
+#mOetqvÚ6:
+#J ÜT/WC0FemíumÜ4À/t7nmeÜaJÚXfó c6Fa2CÀJcrs6 moõãYI7in
+#
+89xJp7rueÚc7(osÚjW6nuk):
+ J J#ÜI.4Ócke parÜÀÜ8uõcroÜeJãYsmak6o PÀ26rJd7 7euXYfq
+m6  79o;rõ õuk B;fKqnI
+
+#Üguõcro: moõãazq
+#
+# tÀ7rm7t/oF:
+#f0Ücníj1:ZP7t6rJcqnu9ázq Õ7 cq;ó;nuoJdÀ 7;WmrdN
+# ÜZm6ns2: PeuXY0mo pen8XÜ;mJc6nóuõãX08emsnt7À8r
+#
+# CeuXYfq:
+#  ÜT7;eJs7 moõáN0Àsóé cq;oBd6 7mÜc6;82ÜemúalF9ÜmaQoJcqnuvÀmào
+#
+dÀÁÜmoõtBdq(mXá9Ã,m2onód):
+J J # S5úãÀmcíta7ZrJf;nmaq 7ZWmqcNL oÜwrão/ üeÜr7ãXmÚo
+6  ÜÁq/ B BnÜrr;Gc(lcí(cq;óY)):
+ Ü JZf0ÜiJ6noo(v7rueõcÀ(mXá9H,m2onós[B])):
+Ü JZf0Ü m6  79o;rõ 4a2s7
+f0Ü kstu7;ÜÍr;e
+
+# 4WáKqez: a82mroJeJsubuvÀKro
+#
+#Ü7r/a.eurqs:
+#0Ü v9nj:ZP7t6rJcqnu9ázq n6coÚáuõt6 PuÀ u9YIÜi 2luFXÜ6uJr7mqvB/X0q c4emÀ;o6
+# J Ün;5:0Àlc7enoXÜr Qe/ rdBÚasÚaK9 ouZ77m6vBdq
+#
+vcx NYicrX(moõjé Úu.):
+Ü m6# I5vãe.eõtrrJÀfBunvéo
+ÜZÜJi8 õoo(99YPÀnvs(cq;óén;m)):
+JZf0Ü m6coÚáhrp9eõd(n;5)
+Ü m6  ÜZm6ns.Qo7t()
+0Ü mLetuvÚ
+
+üe8 FueãYIman(coÚá0Jn;m):
+ JZf#ÜIBilek9Úua/ r xuõÚÀs
+ m6 ixZv7rueõcÀ(mXá90  Fm):
+ÜJ J J Üc6;8á7eB9ve(;u.)
+ J Ü JZfKqnIWso7ã()
+J J 7euWYf
+
+#6FuÚÚq7s: ;nàa6,fGÚtcLsemÀqJeJdBfÀr7;oI
+#
+# Prvr.eur6s:
+#Zf0Ü v9njÃ:ÜAeuo/ moõãCf8om9 cq;ó;nuoJdÀ 7;WmrdN6doÜ47Bm7i/oÜo99YIÚdn
+# ÜZÜJc6ns2: A9Ws7 v9ntÀ;86 6 moÚj;;WsÜdc6enovrüaJd6 FeHWázq nierr;86
+#
+# CeuXYfq:
+#  ÜUÀuo/ moÚt7;vsÜom2onóWÚuoJd7 FaB/Àr7ezFltr/qJdr 6pÀrrÚÀs
+#
+YefÜWÚBa6(moÚjY,fKqnIM):
+ZÜJ # Smvl75Cfoak6a xWÚma6 7 or6ÚÀmÜomHalqvÜüeJr7tqrõX
+0Ü méuxÜ=Ü[c6ns1[i]ZUs7 u6inÜvrõg7(ãeÚ(mXá9Ã))]
+ ÜZÜ8o/ B ànJvÀfZe(4en(Úqõjb)):
+ JZf0Ü médimÀq(a;xé moõáD[à])
+  ÜZr;xFs6ro()
+f0Ü kstu7;ÜruV
+
+dÀfJ2áPÀrzscaq(m6ns1é moõáD):
+m6  #ZI.pãe.eÚtrvfIÜfÕícaqZÀJt/oma7 6ZwI2ok6deÜvÀuo/n6
+Ü JZÀFV =6[]
+ZÜJ 8o/ à B;fmrn0s(lÀ;(moõjb)):
+Zf0Ü m6 ixZm6ns2[i] B;fKqnIá:
+ÜZÜJ J J Ü JÀvGman(auV,Ümoõjb[à])
+f0Ü m6  ÜZÜJa;xFsqru()
+
+ m6 rÀãu/nJa;x
+
+ü9U08iJsreÚÚr(c6ns10 mXá9H):
+  ÜZ#JI.pãekeõãÀmÜammunmÀqJeJt/oma/ZX0PaH9r 89Ü/euo/nq
+JZf0ruL6= []
+J J 8o7 BZafÜrNíge(HÀõ(moõjÃ)):
+0Ü m6  Ü2xJn6t(cqnss[G] uí cq;ób):
+J Ü JZf0Ü m6adàÚr6(ruV,Üc6;8;[i])
+ ÜZÜruV.Qo7t()
+0Ü mLetuvÚJa;x
+
+8e8ZQfàan_diFáuõtr(moÚjY,fKqnIM):
+ZÜJ # Smvl75Cfoak6a xWÚma6 7 or6ÚÀmÜomHalqvÜüeJr7tqrõX
+0Ü méuxÃZ=JdBf7rÀnmÀ(KqnIá,cq;ób)
+ J Üa;üD0= Kwfe79Úma(c6nó2éÚXfó1)
+  ÜZ77t;rõ unBÀX(ruLá,auüH)
